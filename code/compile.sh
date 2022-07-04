4bb63f1fa38133b7d53c5fa45ff0bd20cb6b1b82
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2022-07-05 00:32:38 +0600

    added compiling script

--- code/compile.sh
+++ code/compile.sh
@@ -1,2 +1,4 @@
 echo "require(rmarkdown); require(tinytex); render('medicine.Rmd', output_file='medicine.tex'); pdflatex('medicine.tex')" | R --vanilla
 find . -maxdepth 1 -name "*.pdf" -exec mv {} ../pdfs \;
+
+echo "Done!"
