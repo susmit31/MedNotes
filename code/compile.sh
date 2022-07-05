4880865e69fd228f329a640de157d32c13d6792f
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2022-07-05 10:58:08 +0600

    reconfigured assets

--- code/compile.sh
+++ code/compile.sh
@@ -1,4 +1,4 @@
-echo "require(rmarkdown); require(tinytex); render('medicine.Rmd', output_file='medicine.tex'); pdflatex('medicine.tex')" | R --vanilla
+echo "require(rmarkdown); require(tinytex); render('$1.Rmd', output_file='$1.tex'); pdflatex('$1.tex')" | R --vanilla
 find . -maxdepth 1 -name "*.pdf" -exec mv {} ../pdfs \;
 
 echo "Done!"
