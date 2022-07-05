d60ee26837462d5c986dafa0ac609425630c61fb
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2022-07-05 11:01:16 +0600

    reconfigured compilation script

--- code/compile.sh
+++ code/compile.sh
@@ -1,4 +1,5 @@
 echo "require(rmarkdown); require(tinytex); render('$1.Rmd', output_file='$1.tex'); pdflatex('$1.tex')" | R --vanilla
-find . -maxdepth 1 -name "*.pdf" -exec mv {} ../pdfs \;
+#find . -maxdepth 1 -name "*.pdf" -exec mv {} ../pdfs \;
+ls | grep *.pdf | xargs -I _ mv _ ../pdfs/_
 
 echo "Done!"
