ff29c11cf3d42f872d6339a1c1c662ac691f3c1e
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2022-07-08 20:22:43 +0600

    surgery: added cover

--- code/compile.sh
+++ code/compile.sh
@@ -1,4 +1,9 @@
-echo "require(rmarkdown); require(tinytex); render('$1.Rmd', output_file='$1.tex'); pdflatex('$1.tex')" | R --vanilla
+echo "require(rmarkdown); render('$1.Rmd', output_file='$1.tex')" | R --vanilla
+#sed "s#begin{document}#begin{document}\n\\\includepdf\[pages=1,fitpaper\]{../pdfs/cover-$1.pdf}\n#" "$1.tex" | grep -A4 "begin{document}"
+
+sed "s#\\\frontmatter#\\\frontmatter\n\\\includepdf\[pages=1,fitpaper\]{../pdfs/cover-$1.pdf}#" "$1.tex" > tmp && mv tmp $1.tex && rm tmp
+
+echo "require(tinytex); pdflatex('$1.tex')" | R --vanilla
 #find . -maxdepth 1 -name "*.pdf" -exec mv {} ../pdfs \;
 ls | grep *.pdf | xargs -I _ mv _ ../pdfs/_
 
