dc29551762305cbfc23a3165c65f353e1e833f94
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2022-07-15 19:06:41 +0600

    medicine: CKD staging

--- code/compile.sh
+++ code/compile.sh
@@ -3,6 +3,7 @@ echo "require(rmarkdown); render('$1.Rmd', output_file='$1.tex')" | R --vanilla
 
 sed "s#\\\frontmatter#\\\frontmatter\n\\\includepdf\[pages=1,fitpaper\]{../pdfs/cover-$1.pdf}#" "$1.tex" > tmp && mv tmp $1.tex && rm tmp
 sed "s#pgfplots}#pgfplots}\n\\\usetikzlibrary{positioning,arrows.meta}#" "$1.tex" > tmp && mv tmp $1.tex && rm tmp
+sed "s#\\\usepackage{xcolor}#\\\usepackage\[table\]{xcolor}\n#" "$1.tex" > tmp && mv tmp $1.tex && rm tmp
 
 echo "require(tinytex); pdflatex('$1.tex')" | R --vanilla
 #find . -maxdepth 1 -name "*.pdf" -exec mv {} ../pdfs \;
